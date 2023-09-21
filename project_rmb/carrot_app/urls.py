df7faef7b18d09ffa10c4cb15a847fc7bc6e4e38
Yumi Heo <hymsimple@gmail.com>
2023-09-21 17:12:43 +0900

    template basic settings

--- project_rmb/carrot_app/urls.py
+++ project_rmb/carrot_app/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.main, name='main'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('search/', views.search, name='search'),
+    path('location/', views.location, name='location'),
+    path('trade/', views.trade, name='trade'),
+    path('trade_post/', views.trade_post, name='trade_post'),
+    path('write/', views.write, name='write'),
+    path('chat/', views.chat, name='chat'),
+    path('index/', views.index, name='index'),
+    path('create_form/', views.create_form, name='create_form'),
 ]
--- project_rmb/carrot_app/views.py
+++ project_rmb/carrot_app/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
+def login(request):
+    return render(request, 'registration/login.html')
+
+def register(request):
+    return render(request, 'registration/register.html')
+
+def main(request):
+    return render(request, 'carrot_app/main.html')
+
+def search(request):
+    return render(request, 'carrot_app/search.html')
+
+def location(request):
+    return render(request, 'carrot_app/location.html')
+
+def trade(request):
+    return render(request, 'carrot_app/trade.html')
+
+def trade_post(request):
+    return render(request, 'carrot_app/trade_post.html')
+
+def write(request):
+    return render(request, 'carrot_app/write.html')
+
+def chat(request):
+    return render(request, 'carrot_app/chat.html')
+
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'carrot_app/index.html')
+
+def create_form(request):
+    return render(request, 'carrot_app/create_form.html')
