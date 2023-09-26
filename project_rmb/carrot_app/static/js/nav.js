98f96431e57728acd86064fef72b17e6c9f555af
Hwang-99 <Silr0n0618@naver.com>
2023-09-26 17:53:08 +0900

    [Design] search-clear button

--- project_rmb/carrot_app/static/js/nav.js
+++ project_rmb/carrot_app/static/js/nav.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  if (
-    window.location.pathname.includes("trade") ||
-    window.location.pathname.includes("write") ||
-    window.location.pathname.includes("edit")
-  ) {
-    document.getElementById("trade-button").classList.toggle("orange-text");
-  } else if (window.location.pathname == "/location/") {
-    document.getElementById("location-button").classList.toggle("orange-text");
-  }
+    if (window.location.pathname.includes("trade") || window.location.pathname.includes("write") || window.location.pathname.includes("edit")) {
+        document.getElementById("trade-button").classList.toggle("orange-text");
+    } else if (window.location.pathname == "/location/") {
+        document.getElementById("location-button").classList.toggle("orange-text");
+    }
+});
+
+var buttonClear = document.querySelector(".button-clear");
+buttonClear.addEventListener("click", function () {
+    buttonClear.parentNode.querySelector("input").value = "";
 });
