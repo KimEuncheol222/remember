133c1d36eb6afe546156380688ca002e4443029a
Hwang-99 <Silr0n0618@naver.com>
2023-09-27 11:08:26 +0900

    [Script] 버튼으로 디스플레이 제어 초안

--- project_rmb/carrot_app/static/js/nav.js
+++ project_rmb/carrot_app/static/js/nav.js
@@ -6,7 +6,46 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
-var buttonClear = document.querySelector(".button-clear");
-buttonClear.addEventListener("click", function () {
-    buttonClear.parentNode.querySelector("input").value = "";
+// var buttonClear = document.querySelector(".button-clear");
+// buttonClear.addEventListener("click", function () {
+//     buttonClear.parentNode.querySelector("input").value = "";
+// });
+
+// 버튼을 누를 때 마다 창의 활성화, 비활성화를 반복
+const searchButton = document.querySelector(".search-button");
+const searchContainer = document.querySelector(".search-container");
+const hamburgerButton = document.querySelector(".hamburger-button");
+const hamburgerContainer = document.querySelector(".hamburger-container");
+
+let isSearchContainerVisible = false;
+let isHamburgerContainerVisible = false;
+
+searchButton.addEventListener("click", function () {
+    if (!isSearchContainerVisible) {
+        searchContainer.style.display = "flex";
+        isSearchContainerVisible = true;
+
+        if (isHamburgerContainerVisible) {
+            hamburgerContainer.style.display = "none";
+            isHamburgerContainerVisible = false;
+        }
+    } else {
+        searchContainer.style.display = "none";
+        isSearchContainerVisible = false;
+    }
+});
+
+hamburgerButton.addEventListener("click", function () {
+    if (!isHamburgerContainerVisible) {
+        hamburgerContainer.style.display = "flex";
+        isHamburgerContainerVisible = true;
+
+        if (isSearchContainerVisible) {
+            searchContainer.style.display = "none";
+            isSearchContainerVisible = false;
+        }
+    } else {
+        hamburgerContainer.style.display = "none";
+        isHamburgerContainerVisible = false;
+    }
 });
